7ab661638ae4d029e5ac96b1e4809a20ae512a62
esence <edouard.sence@u-bordeaux.fr>
2023-01-20 08:55:37 +0100

    push pres

---
+++ bin/js.js
@@ -0,0 +1,71 @@
+/* -- Carousel Navigation -- */
+
+let activeIndex = 0;
+
+const slides = document.getElementsByTagName("article");
+
+// From http://stackoverflow.com/a/5365036/2065702
+const randomColor = "#"+((1<<24)*Math.random()|0).toString(16); 
+
+document.documentElement.style.setProperty('--random-color', randomColor);
+
+function invertColor(hexTripletColor) {
+    var color = hexTripletColor;
+    color = color.substring(1);           // remove #
+    color = parseInt(color, 16);          // convert to integer
+    color = 0xFFFFFF ^ color;             // invert three bytes
+    color = color.toString(16);           // convert to hex
+    color = ("000000" + color).slice(-6); // pad with leading zeros
+    color = "#" + color;                  // prepend #
+    return color;
+}
+
+document.documentElement.style.setProperty('--random-font-color', invertColor(randomColor));
+
+const handleLeftClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+        
+  currentSlide.dataset.status = "after";
+  
+  nextSlide.dataset.status = "becoming-active-from-before";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+const handleRightClick = () => {
+  const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+  currentSlide.dataset.status = "before";
+  
+  nextSlide.dataset.status = "becoming-active-from-after";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+/* -- Mobile Nav Toggle -- */
+
+const nav = document.querySelector("nav");
+
+const handleNavToggle = () => {  
+  nav.dataset.transitionable = "true";
+  
+  nav.dataset.toggled = nav.dataset.toggled === "true" ? "false" : "true";
+}
+
+window.matchMedia("(max-width: 800px)").onchange = e => {
+  nav.dataset.transitionable = "false";
+
+  nav.dataset.toggled = "false";
+};
